90a014dbba93a6a2d2a7d29048743a9ef4d56002
danilo Lima Dias <danilo743377@gmail.com>
2021-03-28 15:28:44 -0300

    adicionando botao login e cadastro

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,15 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { Keyboard } from '@ionic-native/keyboard/ngx'
 
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
+  Keyboard
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Keyboard } from '@ionic-native/keyboard/ngx';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { IonSlides } from '@ionic/angular';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +8,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.page.scss'],
 })
 export class LoginPage implements OnInit {
+  @ViewChild(IonSlides) slides: IonSlides;
+  public wavesPosition: number = 0;
+  public wavesDifference: number = 80;
 
-  constructor() { }
 
-  ngOnInit() {
+  constructor(public Keyboard: Keyboard) { }
+
+  ngOnInit() {}
+
+  segmentChanged(event: any) {
+    if (event.detail.value === "login") {
+      this.slides.slidePrev();
+      this.wavesPosition += this.wavesDifference;
+    } else {
+      this.slides.slideNext();
+      this.wavesPosition -= this.wavesDifference;
+
+    }
   }
 
 }
